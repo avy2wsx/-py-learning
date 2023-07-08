176f18ccf51c2ad0d46ef52ee5eb31bce4d5d83b
avy2wsx <136140464+avy2wsx@users.noreply.github.com>
2023-07-08 06:48:42 +0000

    a

--- L6.py
+++ L6.py
@@ -3,6 +3,19 @@
 import random
 
 min = 1
-max = 100
+max = 10
+count = 0
 target = random.randint(min,max)
 print(target)
+
+while True:
+    Keyin =int(input(f"猜數字範圍{min}~{max}:"))
+    count +=1
+    if(keyin ==target):
+        print(f"Bingo,answer is {target}")
+        print(f"總共猜了{count}次")
+        break
+    else:
+        print("wrong")
+        print(f"總共猜了{count}次")
+print("game over")
