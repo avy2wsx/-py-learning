fc1e4ec2371a178fcfbaadac10df7e947f4cb1be
avy2wsx <136140464+avy2wsx@users.noreply.github.com>
2023-07-22 08:22:46 +0000

    a

--- L10-3.py
+++ L10-3.py
@@ -1,5 +1,9 @@
 import requests
 
+def download_weather():
+    url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization=rdec-key-123-45678-011121314&format=JSON'
+    response = requests.get(url)
+    if response.status_code
 
 def main():
     print("main function執行")
